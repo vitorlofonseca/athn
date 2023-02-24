f6bfd9e775014091ff873d08f5aab32bb86cc26d
Vitor Fonseca <vitor.fonseca@kuehne-nagel.com>
2023-02-24 14:05:14 +0000

    Home route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import ComponentsView from "@/views/ComponentsView.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "../views/Login.vue";
+import Login from "@/views/Login.vue";
+import Home from "@/views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
     {
       path: "/",
       name: "login",
