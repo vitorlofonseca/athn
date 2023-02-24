f609c5caf229fab54484ece929ddc9701e1c2040
Vitor Fonseca <vitor.fonseca@kuehne-nagel.com>
2023-02-24 11:02:26 +0000

    Input text with forcing black theme

---
+++ src/components/InputText.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const props = defineProps<{ dark?: Boolean }>();
+</script>
+
+<template>
+  <v-text-field
+    v-bind="$props"
+    :class="{ 'v-input--dark': props.dark }"
+    variant="outlined"
+  ></v-text-field>
+</template>
+
+<style lang="scss" scoped>
+.v-input {
+  color: var(--color-text);
+
+  &--dark {
+    color: var(--vt-c-white-soft);
+  }
+}
+</style>
--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 import Button from "@/components/Button.vue";
+import InputText from "@/components/InputText.vue";
 </script>
 
 <template>
   <main>
     <Button>Button Test</Button>
+    <div class="background-dark">
+      <InputText label="Input dark text example" :dark="true"></InputText>
+    </div>
+    <InputText label="Input text example"></InputText>
   </main>
 </template>
+
+<style lang="scss" scoped>
+main {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.background-dark {
+  padding: 20px;
+  background-color: var(--color-primary);
+}
+</style>
