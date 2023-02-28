6743cb790e029d6c9c0ee5c04147a9e586cb9314
Vitor Fonseca <vitor.fonseca@kuehne-nagel.com>
2023-02-28 15:27:55 +0000

    Home page curation

---
+++ src/components/ArtPreview.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="art-preview">
+    <img src="https://www.dutchamsterdam.nl/i/aa/2007/03/nightwatch2.jpg" />
+    <div class="art-preview__information">
+      <span>
+        <span class="art-preview__name">The night watch</span>
+        <span class="art-preview__artist"> by Rembrandt</span>
+      </span>
+      <div>Date: 1642</div>
+      <div>Dimensions: 363cm x 437cm</div>
+      <div>From Rijksmuseum, Netherlands</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/styling/breakpoints.scss";
+.art-preview {
+  border: 1px solid var(--color-text);
+  width: auto;
+
+  img {
+    width: 100%;
+    max-height: 230px;
+  }
+
+  @media screen and (max-width: $medium) {
+    img {
+      width: 300px;
+    }
+  }
+
+  &__information {
+    display: flex;
+    flex-direction: column;
+    margin: 10px;
+    gap: 5px;
+  }
+
+  &__name {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 20px;
+  }
+
+  &__information,
+  &__artist {
+    font-family: "Arvo";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,92 @@
 <script setup lang="ts">
 import Topbar from "@/components/Topbar/Topbar.vue";
+import ArtPreview from "@/components/ArtPreview.vue";
 </script>
 
 <template>
   <Topbar />
+  <div class="home-body">
+    <h2>Our curation for you</h2>
+
+    <div class="curation">
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+      <ArtPreview></ArtPreview>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import "@/styling/breakpoints.scss";
+
+.home-body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px;
+
+  .curation {
+    display: grid;
+    margin-top: 20px;
+    grid-template-columns: repeat(auto-fill, minmax(290px, 1fr));
+    gap: 20px;
+    justify-items: center;
+    width: 100%;
+    max-width: 1200px;
+  }
+}
+
+@media screen and (max-width: $medium) {
+  .home-body {
+    align-items: unset;
+
+    .curation {
+      display: flex;
+      overflow-x: auto;
+    }
+  }
+}
+</style>
